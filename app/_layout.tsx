8d11bf1591aed122149c34b8cfa1c6a17dd94de2
ddaza <ddaza@users.noreply.github.com>
2025-03-12 21:53:51 -0400

    added theme provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,3 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -10,13 +5,14 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+// import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -32,7 +28,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,34 +1,49 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { ColorSchemeName } from "react-native";
+import type { Theme as ThemeType } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider as ReactThemeProvider,
+} from "@react-navigation/native";
 
-import { ThemeName } from "@/constants/Colors";
+import { THEME_DARK, THEME_LIGHT, ColorSchemeName } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme.web";
 
-type contextType = Maybe<{
+type ThemeContextType = Maybe<{
+  colors: ThemeType;
   theme: ColorSchemeName;
   toggleTheme: () => void;
 }>;
 
-const ThemeContext = createContext<contextType>(undefined);
+const ThemeContext = createContext<ThemeContextType>(undefined);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState(ThemeName.light);
+  const colorScheme = useColorScheme() ?? THEME_LIGHT;
 
-  const colorScheme = useColorScheme() ?? ThemeName.light;
+  const [theme, setTheme] = useState(THEME_LIGHT);
+  const [colors, setColors] = useState(DefaultTheme);
 
   useEffect(() => {
     setTheme(colorScheme);
+    setColors(colorScheme === THEME_DARK ? DarkTheme : DefaultTheme);
   }, [colorScheme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) =>
-      prevTheme === ThemeName.light ? ThemeName.dark : ThemeName.light,
-    );
+    setTheme((prevTheme) => {
+      if (prevTheme === THEME_LIGHT) {
+        return THEME_DARK;
+      }
+      return THEME_LIGHT;
+    });
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+    <ThemeContext.Provider value={{ colors, theme, toggleTheme }}>
+      <ReactThemeProvider
+        value={colorScheme === THEME_DARK ? DarkTheme : DefaultTheme}
+      >
+        {children}
+      </ReactThemeProvider>
     </ThemeContext.Provider>
   );
 };
