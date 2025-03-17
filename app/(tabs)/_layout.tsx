486731e1857d9cdb7eeafe9afb1d1f118d89960d
ddaza <ddaza@users.noreply.github.com>
2025-03-17 16:54:06 -0400

    clean

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,11 +6,9 @@ import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-// import { useColorScheme } from "@/hooks/useColorScheme";
 import { useTheme } from "@/components/ThemeProvider";
 
 export default function TabLayout() {
-  // const colorScheme = useColorScheme();
   const theme = useTheme();
 
   return (
